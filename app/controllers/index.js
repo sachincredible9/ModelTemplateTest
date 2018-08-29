0e19e9be26fe1d50799447433cbe18824778dab3
SACHIN <sachinkumar.sharma@servicemaster.com>
2018-08-28 21:17:45 -0500

    Test iOS12

---
+++ app/controllers/index.js
@@ -0,0 +1,101 @@
+var moment = require('alloy/moment');
+
+var pageSize =5;
+var recordCount = 5000;
+
+$.index.open();
+
+
+var dataColl = Alloy.createCollection('TestModel');
+
+function doClick() {
+	return new Promise(function(resolve, reject){
+		Ti.API.log('Inside doClick');
+		dataColl.reset([]);	
+
+		var i=0 ;
+		for(i=0 ; i < recordCount ; i++ )
+		{
+			var dataSet = Alloy.createModel('TestModel');
+			dataSet.set({
+				Element1: "23e31b5e-7777-11e7-80f3-005056818c8e "+i ,
+				Id: i,
+				 Element2 :  "",
+				 Element3 :"Scheduled"   , 
+	
+	
+				 Element4: "5d3b779b-b7c3-11e7-80f6-00505681f1ce"  ,
+				 Element5:  "5464 Pine Oak Ln" , 
+				 Element6:  " 3647834 " ,
+				 Element7:  "Memphis" ,
+				 Element8:  "TNS" , 
+				 Element9:  "38120" ,
+				 Element10: "35.2218"  , 
+				 Element11:  "-89.88057" ,
+	
+				 Element12:  "eebe5e0c-6b45-471d-b1e7-fb0ba4b4d541" +i ,
+	
+				 Element13 : "19016341512" ,
+				 Element14 :  "Cell",
+				 Element14  : "62f182dc-b572-4a72-97f9-064aca148347",
+				 Element15  : '4898647568945',
+				 Element16  : 'phone',
+				 Element17  : '98547684674956',
+				 Element18  : "primary@gmil.com" ,
+	
+				 Element19  : "2018/08/22", 
+				 Element20  : "08:00", 
+				 Element21   : "2018/08/22",	
+				 Element22   : "17:00",
+				 Element23 :  "$WOISCHSTS",	
+	
+				 Element24  : '57839453536' , 
+				 Element25 :  "$ACTDUR",
+				 Element26 :  "RQS34534",
+				 Element27 :  '387938673956',
+				 Element28 :   "15618415569"			 	    
+			});		
+			dataColl.add(dataSet);		
+			Ti.API.log('...added model ==> '+dataColl.length);	
+		}
+				
+		Promise.delay(1000).then(function(){			
+			resolve(dataColl);
+		});
+		Ti.API.log('Leaving doClick  ==> '+dataColl.length);
+	});
+	
+}
+Ti.API.log('Alloy', 'AppDataDir: ' + Ti.Filesystem.applicationDataDirectory);
+
+function test()
+{
+	Ti.API.log('Alloy', '*********************START TEST**********************' );
+	var arrOfPromise=[];
+	var i=0;
+	for(i; i < pageSize; i++   ){						
+		arrOfPromise.push(doClick());	
+	}
+	
+	Ti.API.log("arrOfPromise.length = "+arrOfPromise.length);
+	 return Promise.map (arrOfPromise,function (response, index, length){
+	 	Ti.API.log('[' + index+ '/'+ length+ ']... Processing...');
+	 	
+	 	Ti.API.log('Before Save: '+dataColl.length);
+	 	var woStartMom = moment();	 	
+	 	dataColl.each(function(model){	 		
+	 		model.save();
+	 	});	 	
+	 	Ti.API.log('...After Save:' + moment().diff(woStartMom) + ' ms');	 	
+	 } ,{concurrency: 1})
+	 .then(function (){
+		Ti.API.log('********All Test executed SUCESSFULLY.********');
+	});
+	
+}
+
+Ti.API.log('*****Start OF TEST ******');
+test().then(function () {
+	Ti.API.log('*****END OF TEST ******');
+});
+
---
+++ app/models/TestModel.js
@@ -0,0 +1,76 @@
+exports.definition = {
+	config: {
+		columns: {
+		   "Element1" : "TEXT PRIMARY KEY",///"text PRIMARY KEY NOT NULL",
+			"Id": "text ", // √
+			"Element2": "text", //TODO - Do we have mapping for this in response?
+			"Element3": "text", // √
+
+
+			"Element4": "text", // √
+			"Element5": "text", // √
+			"Element6": "text", // √
+			"Element7": "text", // √
+			"Element8": "text", // √
+			"Element9": "text", // √
+			"Element10": "text", // √ 
+			"Element11": "text", // √
+
+			"Element12": "text",
+
+			"Element13" : "text",
+			"Element14" : "text",
+			"Element15" : "text",
+			"Element16" : "text",
+			"Element17" : "text",
+			"Element18" : "text",
+			"Element19" : "text",
+
+			"Element20": "text", // √ 
+			"Element21": "text", // √
+			"Element22": "text", // √	
+			"Element23": "text", // √
+			"Element24": "text",	//TODO Parsed from ApptSchedule -> Type
+
+			"Element25": "text", // √ Use the techId we called query for
+			"Element26" : "text",
+			"Element27" : "text",
+			"Element28" : "text",
+			"Element29" : "text"			    
+		},
+		defaults: {
+			"Element24" : "",
+			"Element26" : "-",
+			"Element27" : ""
+		},
+		adapter: {
+			type: "sql",
+			collection_name: "TestModel",
+			idAttribute: "Id"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+
+			// For Backbone v1.1.2, uncomment the following to override the
+			// fetch method to account for a breaking change in Backbone.
+			/*
+			fetch: function(options) {
+				options = options ? _.clone(options) : {};
+				options.reset = true;
+				return Backbone.Collection.prototype.fetch.call(this, options);
+			}
+			*/						
+		});
+
+		return Collection;
+	}
+};
